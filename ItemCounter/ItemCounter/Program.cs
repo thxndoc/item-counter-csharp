b39f4d7fa512487b239b4adddc64c82b50ee557d
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-05-23 20:30:34 +0200

    Call CountItems method

--- ItemCounter/ItemCounter/Program.cs
+++ ItemCounter/ItemCounter/Program.cs
@@ -13,6 +13,19 @@ namespace ItemCounter
             var stringList = new List<string> { "diamond", "sapphire", "diamond", "diamond", "tanzanite", "emerald" };
             var integerList = new List<int> { 7, 4, 6, 6, 5, 2, 4 };
             var characterList = new List<char> { 'd', 's', 'd', 'd', 't', 'e' };
+
+            Console.WriteLine("Count occurence of each string on list:");
+            CountItems(stringList);
+
+            Console.WriteLine("\nInteger list count:");
+            CountItems(integerList);
+
+            Console.WriteLine("\nCharacter list count:");
+            CountItems(characterList);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+
         }
 
         public static void CountItems<T>(IEnumerable<T> items)
