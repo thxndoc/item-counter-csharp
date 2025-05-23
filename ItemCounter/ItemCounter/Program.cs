a85011172a21906896f7d3ba82be3cac790d74e3
Thando Cibi <174618771+thxndoc@users.noreply.github.com>
2025-05-23 20:19:38 +0200

    Add sample list items for different types

---
+++ ItemCounter/ItemCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stringList = new List<string> { "diamond", "sapphire", "diamond", "diamond", "tanzanite", "emerald" };
+            var integerList = new List<int> { 7, 4, 6, 6, 5, 2, 4 };
+            var characterList = new List<char> { 'd', 's', 'd', 'd', 't', 'e' };
+        }
+    }
+}
